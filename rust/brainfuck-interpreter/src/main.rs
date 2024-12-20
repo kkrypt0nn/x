07c2c69261287a2ffb5b6ebbcc83fde4929a8bea
Krypton <root@krypton.ninja>
2024-12-20 16:59:09 +0100

    chore: Fix Brainfuck interpreter running checks

--- rust/brainfuck-interpreter/src/main.rs
+++ rust/brainfuck-interpreter/src/main.rs
@@ -7,12 +7,12 @@ use std::{
 
 fn main() -> io::Result<()> {
     let args = env::args().collect::<Vec<String>>();
-    if args.len() != 2 {
-        println!("Usage: ./bfrs");
+    if args.len() != 2 || !args[1].ends_with(".bf") {
+        println!("Usage: ./bfrs <filename>.bf");
         exit(1);
     }
 
-    let mut file = File::open(args[1].clone())?;
+    let mut file = File::open(&args[1])?;
     let mut buffer = String::new();
     file.read_to_string(&mut buffer)?;
     bfrs::Machine::new().evaluate(buffer);
