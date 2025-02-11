e5ab824038aada334ec2b117478286ec4f4fe585
Krypton <root@krypton.ninja>
2025-02-11 20:00:56 +0100

    feat: Add some container separation

--- rust/imgui/src/main.rs
+++ rust/imgui/src/main.rs
@@ -8,9 +8,6 @@ use sdl2::{
     event::Event,
     video::{GLProfile, Window},
 };
-use uis::{content, main_menu, AppState};
-
-mod uis;
 
 fn glow_context(window: &Window) -> glow::Context {
     unsafe {
@@ -27,7 +24,6 @@ fn main() {
 
     let window = video_subsystem
         .window("Example App", 1280, 720)
-        .allow_highdpi()
         .opengl()
         .position_centered()
         .resizable()
@@ -49,10 +45,10 @@ fn main() {
     let mut platform = SdlPlatform::new(&mut imgui);
     let mut renderer = AutoRenderer::new(gl, &mut imgui).unwrap();
 
-    let mut app_state = AppState::MainMenu;
     let mut running = true;
-
+    let mut tab_open = true;
     let mut event_pump = sdl.event_pump().unwrap();
+
     while running {
         for event in event_pump.poll_iter() {
             platform.handle_event(&mut imgui, &event);
@@ -64,10 +60,55 @@ fn main() {
         platform.prepare_frame(&mut imgui, &window, &event_pump);
         let ui = imgui.new_frame();
 
-        match app_state {
-            AppState::MainMenu => main_menu::render(ui, &mut app_state, &mut running),
-            AppState::Content => content::render(ui, &mut running),
+        if let Some(menu_bar) = ui.begin_main_menu_bar() {
+            if let Some(file_menu) = ui.begin_menu("File") {
+                if ui.menu_item("Hello") {
+                    println!("world!");
+                }
+                file_menu.end();
+            }
+            if let Some(help_menu) = ui.begin_menu("Help") {
+                if ui.menu_item("Help") {
+                    println!("no help :(");
+                }
+                help_menu.end();
+            }
+            menu_bar.end();
         }
+
+        ui.window("Example App")
+            .size(ui.io().display_size, Condition::Always)
+            .position([0.0, 20.0], Condition::Always)
+            .movable(false)
+            .resizable(false)
+            .title_bar(false)
+            .build(|| {
+                ui.columns(2, "MainColumns", true);
+                ui.text("Right");
+                ui.next_column();
+
+                if let Some(tab_bar) = ui.tab_bar("tab") {
+                    if tab_open {
+                        if let Some(_tab) = ui.tab_item_with_flags(
+                            "one",
+                            Some(&mut tab_open),
+                            TabItemFlags::empty(),
+                        ) {
+                            let mut text_buffer = String::new();
+                            ui.input_text_multiline(
+                                "##",
+                                &mut text_buffer,
+                                ui.content_region_avail(),
+                            )
+                            .build();
+                        }
+                    }
+                    if let Some(_tab) = ui.tab_item("two") {
+                        ui.text_colored([255.0, 0.0, 0.0, 255.0], "Error?");
+                    }
+                    tab_bar.end();
+                }
+            });
 
         let draw_data = imgui.render();
         unsafe { renderer.gl_context().clear(glow::COLOR_BUFFER_BIT) };
--- rust/imgui/src/uis/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod content;
-pub mod main_menu;
-
-pub enum AppState {
-    MainMenu,
-    Content,
-}
